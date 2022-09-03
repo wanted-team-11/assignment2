11752dc0a2a0f40052b9273f086b4e099ef32c59
정재훈 <jhc060109@naver.com>
2022-09-03 23:34:18 +0900

    feat: order list 페이지 컴포넌트 생성 (미완성)

--- src/pages/order-list/index.tsx
+++ src/pages/order-list/index.tsx
@@ -1,4 +1,40 @@
+import styled from "styled-components";
+import { useState, useEffect } from "react";
+import { getConfirmedOrders } from "../../api/product.api";
+
+/**
+ * TODO: 미완성. 개발 진행 요망
+ */
 const OrderListPage = () => {
-  return <div>OrderListPage</div>;
+  const [orderList, setOrderList] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const res = await getConfirmedOrders();
+      // setOrderList(res);
+    })();
+  }, []);
+  return (
+    <S.Container>
+      <S.Wrapper>OrderListPage</S.Wrapper>
+    </S.Container>
+  );
 };
 export default OrderListPage;
+
+const S: any = {};
+
+S.Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  background-color: #f3f3f3;
+  & * {
+    box-sizing: border-box;
+  }
+`;
+
+S.Wrapper = styled.div`
+  max-width: 860px;
+`;
