70dadde1a232f43c1a981ae697e4962f8915c5eb
정재훈 <jhc060109@naver.com>
2022-09-03 23:32:14 +0900

    chore: mock-up 데이터 추가 및 pseudo api 연동
    * 기존의 order-completed.json 목업 데이터 => 다른 용도의 데이터일 수 있어
    임시로 주문 확인 페이지의 mock-up데이터 추가
    
    * pseudo api와 연동

--- src/api/product.api.ts
+++ src/api/product.api.ts
@@ -2,3 +2,8 @@ export const getData = async (id: number) => {
   const res = await import("../mockup-data/products.json");
   console.log(res.default[id]);
 };
+
+export const getConfirmedOrders = async () => {
+  const res = await import("../mockup-data/confirmed-orders.json");
+  return res.default;
+};
