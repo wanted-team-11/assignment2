eab70d3ac6162d5e20657fd8806bfd81f0a1f418
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2022-09-04 21:11:11 +0900

    feat: 상품 등록 옵션 리스트 구현

--- src/pages/admin/product-upload/components/OptionItem.tsx
+++ src/pages/admin/product-upload/components/OptionItem.tsx
@@ -1,3 +1,5 @@
+import styled from "styled-components";
+
 interface Option {
   stockCount: number;
   name: string;
@@ -13,14 +15,36 @@ const OptionItem = ({ option, onClickRemoveOption }: OptionItemProps) => {
     onClickRemoveOption(option);
   };
   return (
-    <li>
-      <div>
-        <p>옵션: {option.name}</p>
-        <p>가격: {option.price}원</p>
-        <p>수량: {option.stockCount}</p>
-      </div>
-      <button onClick={onClickOption}>X</button>
-    </li>
+    <tr>
+      <TD>{option.name}</TD>
+      <TD>{option.price}원</TD>
+      <TD>{option.stockCount}개</TD>
+      <TD>
+        <RemoveButton onClick={onClickOption}>삭제</RemoveButton>
+      </TD>
+    </tr>
   );
 };
 export default OptionItem;
+
+const TD = styled.td`
+  border-bottom: 1px solid #444444;
+  padding: 10px;
+  text-align: center;
+`;
+
+const RemoveButton = styled.button`
+  width: 60px;
+  height: 40px;
+  border: none;
+  border-radius: 5px;
+  background-color: #fa5858;
+  color: white;
+  font-size: 14px;
+  display: inline-block;
+  font-weight: 400;
+  line-height: 1.5;
+  text-align: center;
+  text-decoration: none;
+  cursor: pointer;
+`;
