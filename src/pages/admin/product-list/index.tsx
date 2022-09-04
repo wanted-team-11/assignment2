fef8b9cefc064c0b1fcd836d7e9b268efefa94c5
happyeveryone96 <happyeveryone@naver.com>
2022-09-04 23:49:26 +0900

    refactor: 분리되어 있는 관리자 상품 페이지 스타일 코드 병합

--- src/pages/admin/product-list/index.tsx
+++ src/pages/admin/product-list/index.tsx
@@ -2,7 +2,7 @@ import ProductItem from "../../../components/AdminProductItem";
 import useRemoveProduct from "../../../hooks/useRemoveProduct";
 import useToggleProduct from "../../../hooks/useToggleProduct";
 import { Product } from "../../../types/types";
-import * as S from "../../../components/styles/AdminProductList.styled";
+import styled from "styled-components";
 
 const AdminProductListPage = () => {
   const {
@@ -23,18 +23,18 @@ const AdminProductListPage = () => {
   } = useToggleProduct();
 
   return (
-    <S.Wrapper>
-      <S.PaddingButton>
-        <S.Button onClick={onAllBehind}>전체 숨김</S.Button>
-        <S.Button onClick={showAllProduct}>전체 노출</S.Button>
-        <S.Button onClick={onAllRemove}>전체 삭제</S.Button>
-      </S.PaddingButton>
-      <S.PaddingButton>
-        <S.Button onClick={hideCheckedProduct}>선택된 항목 숨김</S.Button>
-        <S.Button onClick={showCheckedProduct}>선택된 항목 노출</S.Button>
-        <S.Button onClick={ChooseRemove}>선택된 항목 삭제</S.Button>
-      </S.PaddingButton>
-      <S.ProductListContainer>
+    <Wrapper>
+      <PaddingButton>
+        <Button onClick={onAllBehind}>전체 숨김</Button>
+        <Button onClick={showAllProduct}>전체 노출</Button>
+        <Button onClick={onAllRemove}>전체 삭제</Button>
+      </PaddingButton>
+      <PaddingButton>
+        <Button onClick={hideCheckedProduct}>선택된 항목 숨김</Button>
+        <Button onClick={showCheckedProduct}>선택된 항목 노출</Button>
+        <Button onClick={ChooseRemove}>선택된 항목 삭제</Button>
+      </PaddingButton>
+      <ProductListContainer>
         {productList?.map((product: Product) => {
           return (
             <ProductItem
@@ -47,9 +47,41 @@ const AdminProductListPage = () => {
             />
           );
         })}
-      </S.ProductListContainer>
-    </S.Wrapper>
+      </ProductListContainer>
+    </Wrapper>
   );
 };
 
 export default AdminProductListPage;
+
+export const Wrapper = styled.div`
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  position: relative;
+`;
+
+export const PaddingButton = styled.div`
+  display: flex;
+  padding: 15px 0px 5px 27px;
+  margin: 0 22.5vw;
+  gap: 10px;
+`;
+
+export const ProductListContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 4px;
+  width: fit-content;
+  height: 50px;
+  border: 1px solid gray;
+  border-radius: 4px;
+  cursor: pointer;
+  background-color: white;
+`;
