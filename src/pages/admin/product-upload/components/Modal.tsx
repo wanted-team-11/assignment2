2abd2a104f899cb99871a801f40033520d75b590
gkdfo40 <gkdfo40@gmail.com>
2022-09-04 15:34:51 +0900

    feat: 옵션 추가 모달 생성 onClickOutside 커스텀 훅을 생성하여 모달 content이외의 클릭시 모달 닫기 기능 추가

--- src/pages/admin/product-upload/components/Modal.tsx
+++ src/pages/admin/product-upload/components/Modal.tsx
@@ -1,52 +1,43 @@
-import React, { ReactNode, Dispatch } from "react";
+import { ReactNode, Dispatch, useRef } from "react";
 import styled from "styled-components";
+import useOnClickOutside from "../hooks/useOnClickOutside";
 
-type ModalProps = {
+interface ModalProps {
   setModalState: Dispatch<boolean>;
   children: ReactNode;
-};
+}
 
 const Modal = ({ setModalState, children }: ModalProps) => {
+  const ModalRef = useRef<HTMLDivElement>(null);
   const onClickCloseModal = () => {
     setModalState(false);
   };
 
+  useOnClickOutside(ModalRef, () => setModalState(false));
+
   return (
-    <ModalContainer onClick={onClickCloseModal}>
-      <ModalBackground>
-        <ModalBox>
-          <div>{children}</div>
-          <button onClick={onClickCloseModal}>X</button>
-        </ModalBox>
-      </ModalBackground>
-    </ModalContainer>
+    <ModalBackground>
+      <ModalBox ref={ModalRef}>
+        <div>{children}</div>
+        <button onClick={onClickCloseModal}>X</button>
+      </ModalBox>
+    </ModalBackground>
   );
 };
 
 export default Modal;
 
-const ModalContainer = styled.div`
+const ModalBackground = styled.div`
   position: fixed;
   top: 0;
   left: 0;
+  z-index: 1000;
   width: 100%;
-  height: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
   display: flex;
-  flex-flow: row wrap;
-  justify-content: center;
   align-items: center;
-`;
-
-const ModalBackground = styled.div`
-  width: 100%;
-  height: 100%;
-  background-color: #000;
-  opacity: 0.5;
-  position: fixed;
-  display: flex;
-  flex-flow: row wrap;
   justify-content: center;
-  align-items: center;
 `;
 
 const ModalBox = styled.div`
