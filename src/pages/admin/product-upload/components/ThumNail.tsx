18e46dedba7af9ad0da674e25dff09d10e722706
gkdfo40 <gkdfo40@gmail.com>
2022-09-04 21:17:39 +0900

    style: thumbNail 컴포넌트 스타일 수정 및 삭제 버튼 추가

--- src/pages/admin/product-upload/components/ThumNail.tsx
+++ src/pages/admin/product-upload/components/ThumNail.tsx
@@ -1,3 +1,5 @@
+import styled from "styled-components";
+
 interface ThumbNailProps {
   item: string;
   onClickRemoveImg: (id: string) => void;
@@ -7,10 +9,32 @@ const ThumbNail = ({ item, onClickRemoveImg }: ThumbNailProps) => {
     onClickRemoveImg(item);
   };
   return (
-    <li onClick={onClickItem}>
-      <img src={item} alt="thumbnail" width={100} height={100} />
-    </li>
+    <ImgContainer>
+      <img src={item} alt="thumbnail" width={200} height={200} />
+      <button onClick={onClickItem}>-</button>
+    </ImgContainer>
   );
 };
 
 export default ThumbNail;
+
+const ImgContainer = styled.li`
+  position: relative;
+  width: 200px;
+  height: 200px;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.3);
+  :hover {
+    opacity: 0.8;
+    box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+  }
+  img {
+    object-fit: cover;
+  }
+
+  button {
+    position: absolute;
+    top: 1px;
+    right: 1px;
+    cursor: pointer;
+  }
+`;
