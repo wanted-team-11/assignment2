ac5cf4ac3f990e07284511380e2c1f8ec1a7d530
정재훈 <jhc060109@naver.com>
2022-09-05 10:44:58 +0900

    fix: order 페이지에서 상품 이미지 안뜨는 버그 재수정
    json 파일명 접근 방식 수정

--- src/api/order.api.ts
+++ src/api/order.api.ts
@@ -1,10 +1,13 @@
 import type { Product } from "../types/orderTypes";
 
 export const fetchProductDetail = async (id: number) => {
-  const [products, error] = await getFetch(`/mockup-data/products.json`);
+  const [products, error] = await getFetch(
+    `/mockup-data/limit=10&offset=${Math.floor((id - 1) / 10) * 10}.json`
+  );
   if (error) {
     // TODO: handle error
   }
+  console.log(id);
   return products.filter((product: Product) => product.id === id)[0];
 };
 
